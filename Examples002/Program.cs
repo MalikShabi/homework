2ebd6ff1152d45196383afff8e862f0f6289599d
Malik Shabi <malikshabi@yandex.ru>
2022-11-01 22:05:27 +0300

    second ex

---
+++ Examples002/Program.cs
@@ -0,0 +1,22 @@
+﻿///Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.WriteLine("Inter the first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Inter the second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Inter the third number: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+
+    if (num1 > num2)
+    {
+        max = num1;
+    }
+    else
+    {
+        max = num2;
+    }
+    if (max < num3)
+    {
+        max = num3;
+    }
+    Console.WriteLine("Maximum number: " + max);
