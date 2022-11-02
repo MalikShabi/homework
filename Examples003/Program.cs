42d0b089ccd7bd3ebbe8da6fc44a78a1ee7dc394
Malik Shabi <malikshabi@yandex.ru>
2022-11-02 16:54:50 +0300

    add ex003

---
+++ Examples003/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+Console.Write("Enter a number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num % 2 == 0)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
+
+
