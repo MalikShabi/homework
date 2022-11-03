422e2e4b3070c364337c349b012ea22da1f44d7c
Malik Shabi <malikshabi@yandex.ru>
2022-11-03 17:37:30 +0300

    add ex006

---
+++ HW_2/Examples006/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.WriteLine("Enter a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int digit = 0;
+if (number >= 100)
+{
+    while (number > 999)
+    {
+        number /= 10;
+    }
+    digit = number % 10;
+    Console.WriteLine(digit);
+}
+else
+{
+    Console.WriteLine("There is no third digit: ");
+}
