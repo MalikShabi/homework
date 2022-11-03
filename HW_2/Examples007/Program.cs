86fb4c53e0cff2533e6e854ac3d4e499304dbd4b
Malik Shabi <malikshabi@yandex.ru>
2022-11-03 19:14:12 +0300

    add ex007

---
+++ HW_2/Examples007/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+Console.WriteLine("Enter the day number of the week: ");
+int day = Convert.ToInt32(Console.ReadLine());
+if (day > 7)
+{
+    Console.WriteLine("There is no such day in a week!");
+}
+else if (day == 5 || day == 6)
+    {
+    Console.WriteLine("Weekend!!!");
+    }
+else
+    {
+    Console.WriteLine("To work!");
+    }
