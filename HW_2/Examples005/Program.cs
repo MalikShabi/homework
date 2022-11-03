60d4800f51b3f5cd6939bf67fd7dafb02d608b47
Malik Shabi <malikshabi@yandex.ru>
2022-11-03 16:43:03 +0300

    add ex005

---
+++ HW_2/Examples005/Program.cs
@@ -0,0 +1,5 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.WriteLine("Enter three-digit number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int figure = number / 10 % 10;
+Console.WriteLine(figure);
