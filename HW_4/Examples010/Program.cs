580dc68e138170b81e3d0bd1e01f68fad726ce6f
Malik Shabi <malikshabi@yandex.ru>
2022-11-07 20:08:52 +0300

    add ex010

---
+++ HW_4/Examples010/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите метод, который принимает на вход два числа (A и B)
+// и возводит число A в целую степень B.
+Console.WriteLine("Enter a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter a number: ");
+int degree = Convert.ToInt32(Console.ReadLine());
+double calculation (int number, int degree)
+{
+    bool flag = degree > 0;
+    if (degree < 0) degree = - degree;
+    int result = 1;
+    for (int i = 0; i < degree; i++) result *= number;
+    if (flag) return result;
+    else return 1.0 / result;
+}
+double output = calculation (number, degree);
+Console.WriteLine ($"Number {number} in degree {degree} alike {output}");
