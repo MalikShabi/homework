5b68bfe06ea7de67dbacf397b637599d2bf690c9
Malik Shabi <malikshabi@yandex.ru>
2022-11-07 20:58:43 +0300

    add ex012

---
+++ HW_4/Examples012/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая задаёт массив из N элементов
+// и выводит их на экран.
+Console.WriteLine("Enter a number: ");
+int count = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[count];
+Console.WriteLine("Massif: ");
+for (int i = 0; i < count + 1; i++)
+{
+    array[i] = new Random().Next(count + 1);
+    Console.WriteLine($"{array[i]}; ");
+}
