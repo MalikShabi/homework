5ed77daef2041c796730da93dd0ab88175c29ba1
Malik Shabi <malikshabi@yandex.ru>
2022-11-07 20:57:49 +0300

    add ex011

---
+++ HW_4/Examples011/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход число
+// и выдаёт сумму цифр в числе.
+Console.WriteLine("Enter a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int calculate(int number)
+{
+    int temp = 0;
+    int summ = 0;
+    while (number > 0)
+    {
+        temp = number % 10;
+        summ = summ + temp;
+        number = number / 10;
+    }
+    return summ;
+}
+int allsumm = calculate(number);
+Console.WriteLine($"Summ of all numbers: {allsumm}");
