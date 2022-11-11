b29cc84f479e94379b7436ffefafeae18304bcc6
Malik Shabi <malikshabi@yandex.ru>
2022-11-11 17:45:29 +0300

    add HW06

---
+++ HW_6/Examples016/Program.cs
@@ -0,0 +1,6 @@
+﻿//Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+Console.WriteLine("Enter numbers by a space: ");
+int[] array = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = array.Count(x => x > 0);
+Console.WriteLine($"Number of elements > 0: {count}");
---
+++ HW_6/Examples017/Program.cs
@@ -0,0 +1,26 @@
+﻿//Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+double pointX;
+double pointY;
+
+Console.Write("Lines by formulas y = k1 * x + b1, y = k2 * x + b2");
+Console.Write("Enter in turn b1, k1, b2 и k2 by space: ");
+int[] array = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+int lineA = array [1];
+int lineB = array [3];
+int lineC = array [0];
+int lineD = array [2];
+
+if (lineA == lineB)
+{
+    Console.WriteLine("lines are parallel");
+}
+else
+{
+    pointX = (double) (lineD-lineC)/(lineA-lineB);
+    pointY = (double) lineA*(lineD-lineC)/(lineA-lineB) + lineC;
+    Console.WriteLine("Intersection of two lines:");
+    Console.WriteLine($"Х: {pointX:0.0}");
+    Console.WriteLine($"Y: {pointY:0.0}");
+}
